1e4071b16278978fd91d979bdc6e8c5bb0719b7e
Yixue Zhao <felicitia2010@gmail.com>
2021-07-18 14:35:02 -0400

    update to only compute consecutive sentence sim

--- util/point_similarity.py
+++ util/point_similarity.py
@@ -32,7 +32,7 @@ def merge_sentencetokens(review_text_tokenized):
         merged_paragraphs.append(merged_paragraph)
     return merged_paragraphs
 
-def computer_sentence_sim(sentence_list):
+def computer_consecutiveSentences_sim(sentence_list):
     # Encode all sentences
     embeddings = model.encode(sentence_list)
 
@@ -40,21 +40,20 @@ def computer_sentence_sim(sentence_list):
     cos_sim = util.pytorch_cos_sim(embeddings, embeddings)
 
     # Add all pairs to a list with their cosine similarity score
-    all_sentence_combinations = []
+    all_consecutive_sentences = []
     for i in range(len(cos_sim) - 1):
-        for j in range(i + 1, len(cos_sim)):
-            all_sentence_combinations.append([cos_sim[i][j], i, j])
+        all_consecutive_sentences.append([cos_sim[i][i+1], i, i+1])
 
-    # Sort list by the highest cosine similarity score
-    all_sentence_combinations = sorted(all_sentence_combinations, key=lambda x: x[0], reverse=True)
-
-    print("Top-5 most similar pairs:")
-    for score, i, j in all_sentence_combinations:
-        print("{:.4f} \n {} \n {}".format(cos_sim[i][j], sentence_list[i], sentence_list[j]))
+    print("similarity of each consecutive pairs:")
+    for score, i, j in all_consecutive_sentences:
+        if score >= 0.5:
+            print("{:.4f} \n {} \n {}".format(cos_sim[i][j], sentence_list[i], sentence_list[j]))
 
 if __name__ == '__main__':
     reviewset_array = read_input(input_file)
-    review_text_tokenized = (reviewset_array[0].reviews)[0].review_text_tokenized
-    merged_paragraphs = merge_sentencetokens(review_text_tokenized)
-    for paragraph in merged_paragraphs:
-        computer_sentence_sim(paragraph)
+    for reviewset in reviewset_array:
+        print(reviewset.paper_title)
+        for review in reviewset.reviews:
+            merged_paragraphs = merge_sentencetokens(review.review_text_tokenized)
+            for paragraph in merged_paragraphs:
+                computer_consecutiveSentences_sim(paragraph)
